0c6bfd63c9ee708a3cfd50e79c448a406df41251
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-05-16 07:28:34 -0700

    update buitins.c

--- builtins.c
+++ builtins.c
@@ -3,17 +3,27 @@
 #include <string.h>
 #include "shell.h"
 
+/**
+ * execute_builtin_command - takes a command and performs
+ * the corresponding action for the built-in command.
+ * @command: param command.
+ * Return: no return.
+ */
+
 void execute_builtin_command(char *command)
 {
-    if (strcmp(command, "exit") == 0) {
-        exit(0);
-    }
-    else if (strcmp(command, "env") == 0) {
-        char **env = environ;
-        while (*env) {
-            printf("%s\n", *env);
-            env++;
-        }
-    }
-    // Add more built-in commands here...
+	if (strcmp(command, "exit") == 0)
+	{
+		exit(0);
+	}
+	else if (strcmp(command, "env") == 0)
+	{
+		char **env = environ;
+
+		while (*env)
+		{
+			printf("%s\n", *env);
+			env++;
+		}
+	}
 }
