f263ba379b5c70eb43638c9f1ca8d4a76fc00740
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-05-16 07:41:21 -0700

    update cd.c

--- cd.c
+++ cd.c
@@ -3,37 +3,52 @@
 #include <unistd.h>
 #include "shell.h"
 
+/**
+ * execute_builtin_command - takes a command and perfoms
+ * the corresponding action for the built-in command.
+ * @command: param command line.
+ * Return: no return.
+ */
+
 void execute_builtin_command(char *command)
 {
-    if (strncmp(command, "cd", 2) == 0) {
-        char *arg = strtok(command, " ");
-        arg = strtok(NULL, " ");
-        
-        if (arg == NULL || strcmp(arg, "~") == 0) {
-            // No argument or argument is "~" (home directory)
-            char *home = getenv("HOME");
-            if (home == NULL) {
-                fprintf(stderr, "cd: No home directory\n");
-                return;
-            }
-            if (chdir(home) != 0) {
-                perror("cd");
-            }
-        } else if (strcmp(arg, "-") == 0) {
-            // Argument is "-"
-            char *previous = getenv("OLDPWD");
-            if (previous == NULL) {
-                fprintf(stderr, "cd: No previous directory\n");
-                return;
-            }
-            if (chdir(previous) != 0) {
-                perror("cd");
-            }
-        } else {
-            // Change to the specified directory
-            if (chdir(arg) != 0) {
-                perror("cd");
-            }
-        }
-    }
+	if (strncmp(command, "cd", 2) == 0)
+	{
+		char *arg = strtok(command, " ");
+
+		arg = strtok(NULL, " ");
+		if (arg == NULL || strcmp(arg, "~") == 0)
+		{
+			char *home = getenv("HOME");
+
+			if (home == NULL)
+			{
+				fprintf(stderr, "cd: No home directory\n");
+				return;
+			}
+			if (chdir(home) != 0)
+			{
+				perror("cd");
+			}
+		} else if (strcmp(arg, "-") == 0)
+		{
+			char *previous = getenv("OLDPWD");
+
+			if (previous == NULL)
+			{
+				fprintf(stderr, "cd: No previous directory\n");
+				return;
+			}
+			if (chdir(previous) != 0)
+			{
+				perror("cd");
+			}
+		} else
+		{
+			if (chdir(arg) != 0)
+			{
+				perror("cd");
+			}
+		}
+	}
 }
