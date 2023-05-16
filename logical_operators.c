d41facee454309e349af91247c28bc49a8616bc4
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-05-16 09:32:09 -0700

    update logical_operators.c

--- logical_operators.c
+++ logical_operators.c
@@ -3,33 +3,38 @@
 #include <string.h>
 #include "shell.h"
 
+/**
+ * execute_command - execute a single command within the shell.
+ * @command: param command.
+ * Return: return exit status integer.
+ */
+
 void execute_command(char *command)
 {
-    // Check if the command contains the logical operators && or ||
-    char *and_operator = strstr(command, "&&");
-    char *or_operator = strstr(command, "||");
+	char *and_operator = strstr(command, "&&");
+	char *or_operator = strstr(command, "||");
 
-    if (and_operator != NULL) {
-        // Execute the first command before &&
-        *and_operator = '\0'; // Replace && with null terminator
-        execute_command(command);
+	if (and_operator != NULL)
+	{
+		*and_operator = '\0';
+		execute_command(command);
 
-        // Execute the second command after &&
-        char *next_command = and_operator + 2;
-        execute_command(next_command);
-    } else if (or_operator != NULL) {
-        // Execute the first command before ||
-        *or_operator = '\0'; // Replace || with null terminator
-        execute_command(command);
+		char *next_command = and_operator + 2;
 
-        // Execute the second command after ||
-        char *next_command = or_operator + 2;
-        execute_command(next_command);
-    } else {
-        // No logical operator, execute the command as usual
-        if (is_builtin_command(command))
-            execute_builtin_command(command);
-        else
-            printf("Command not found: %s\n", command);
-    }
+		execute_command(next_command);
+	} else if (or_operator != NULL)
+	{
+		*or_operator = '\0';
+		execute_command(command);
+
+		char *next_command = or_operator + 2;
+
+		execute_command(next_command);
+	} else
+	{
+		if (is_builtin_command(command))
+			execute_builtin_command(command);
+		else
+			printf("Command not found: %s\n", command);
+	}
 }
