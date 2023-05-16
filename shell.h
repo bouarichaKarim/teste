34ddb4eab61d11b8ce92879f14183bf53802611a
KARIM BOUARICHA <113786802+bouarichaKarim@users.noreply.github.com>
2023-05-16 14:39:36 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,6 +1,22 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct alias_s
+{
+    char *name;
+    char *value;
+    struct alias_s *next;
+} alias_t;
+
+void print_alias(alias_t *alias_list);
+int add_alias(alias_t **alias_list, const char *name, const char *value);
+char *find_alias(alias_t *alias_list, const char *name);
+
 void prompt(void);
 void execute_command(char *command);
 int is_builtin_command(char *command);
