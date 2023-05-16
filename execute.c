017380b56f1ce9006610eadf007ff1b71eb4a9f2
KARIM BOUARICHA <113786802+bouarichaKarim@users.noreply.github.com>
2023-05-16 15:02:13 +0100

    Update execute.c

--- execute.c
+++ execute.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "shell.h"
 
+/**
+* execute_command - execute commands.
+* @command: param command to be executed.
+* Return: no return.
+*/
 void execute_command(char *command)
 {
 	if (is_builtin_command(command))
@@ -10,10 +15,24 @@ void execute_command(char *command)
 		printf("Command not found: %s\n", command);
 }
 
+/**
+* is_builtin_command - test if the command is builtin.
+* @command: param command to be executed.
+* Return: 1 if it is a built-in command, or 0 if it is not.
+*/
+
 int is_builtin_command(char *command)
 {
 	return (strcmp(command, "exit") == 0 || strcmp(command, "env") == 0);
 }
+
+
+/**
+* execute_builtin_command - supports the exit command 
+* for exiting the shell.
+* @command: param command to be executed.
+* Return: no return.
+*/
 
 void execute_builtin_command(char *command)
 {
