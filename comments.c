030a55e448c52c7086ff58a7015c8f09587b53e1
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-05-16 08:48:44 -0700

    update comments.c

--- comments.c
+++ comments.c
@@ -3,15 +3,20 @@
 #include <string.h>
 #include "shell.h"
 
+/**
+ * execute_command - execute command, and handles both
+ * regular commands and comments.
+ * @command: param command line.
+ * Return: no return.
+ */
+
 void execute_command(char *command)
 {
-    // Ignore comments (lines starting with '#')
-    if (command[0] == '#')
-        return;
+	if (command[0] == '#')
+		return;
 
-    // Execute the command as usual
-    if (is_builtin_command(command))
-        execute_builtin_command(command);
-    else
-        printf("Command not found: %s\n", command);
+	if (is_builtin_command(command))
+		execute_builtin_command(command);
+	else
+		printf("Command not found: %s\n", command);
 }
