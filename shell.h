6780d6e2cc4bd33f915096db3ea9c65d8ae44352
KARIM BOUARICHA <113786802+bouarichaKarim@users.noreply.github.com>
2023-05-20 15:18:42 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -37,10 +37,8 @@ void execute_command_setenv_unsetenv(char *command)
 int is_builtin_command(char *command);
 void execute_builtin_command_variables(char *command);
 void execute_builtin_command_builtins(char *command);
-char **parse_arguments_tokenize(char *line);
-void free_arguments_tokenize(char **arguments);
-char **parse_arguments_arguments(char *line);
-void free_arguments_arguments(char **arguments);
+char **parse_arguments(char *line);
+void free_arguments(char **arguments);
 char *create_command_path(char *command, char *directory);
 char *get_command_path(char *command);
 void execute_commands_from_file(const char *filename);
