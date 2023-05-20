08f499aa13eafe236aafde94caddea2f0a0126cf
bouarichaKarim <bouaaricha.karim@gmail.com>
2023-05-20 05:55:16 -0700

    update arguments.c

--- arguments.c
+++ arguments.c
@@ -11,32 +11,32 @@
  */
 char **parse_arguments(char *line)
 {
-    char **arguments = NULL;
-    char *token;
-    int arg_count = 0;
-    int arg_size = 2;
+	char **arguments = NULL;
+	char *token;
+	int arg_count = 0;
+	int arg_size = 2;
 
-    arguments = malloc(sizeof(char *) * arg_size);
+	arguments = malloc(sizeof(char *) * arg_size);
 
-    token = strtok(line, " ");
-    while (token != NULL)
-    {
-        arguments[arg_count] = malloc(strlen(token) + 1);
-        strcpy(arguments[arg_count], token);
-        arg_count++;
+	token = strtok(line, " ");
+	while (token != NULL)
+	{
+		arguments[arg_count] = malloc(strlen(token) + 1);
+		strcpy(arguments[arg_count], token);
+		arg_count++;
 
-        if (arg_count >= arg_size)
-        {
-            arg_size += 2;
-            arguments = realloc(arguments, sizeof(char *) * arg_size);
-        }
+		if (arg_count >= arg_size)
+		{
+			arg_size += 2;
+			arguments = realloc(arguments, sizeof(char *) * arg_size);
+		}
 
-        token = strtok(NULL, " ");
-    }
+		token = strtok(NULL, " ");
+	}
 
-    arguments[arg_count] = NULL;
+	arguments[arg_count] = NULL;
 
-    return arguments;
+	return (arguments);
 }
 
 /**
@@ -47,13 +47,13 @@ char **parse_arguments(char *line)
  */
 void free_arguments(char **arguments)
 {
-    int i = 0;
+	int i = 0;
 
-    while (arguments[i] != NULL)
-    {
-        free(arguments[i]);
-        i++;
-    }
+	while (arguments[i] != NULL)
+	{
+		free(arguments[i]);
+		i++;
+	}
 
-    free(arguments);
+	free(arguments);
 }
