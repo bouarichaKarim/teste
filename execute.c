e71afcdbf14dc4f2860f93f62e8d3a72f3379f82
KARIM BOUARICHA <113786802+bouarichaKarim@users.noreply.github.com>
2023-05-20 14:25:19 +0100

    Update execute.c

--- execute.c
+++ execute.c
@@ -4,18 +4,6 @@
 
 
 /**
- * is_builtin_command - test if the command is builtin.
- * @command: param command to be executed.
- * Return: 1 if it is a built-in command, or 0 if it is not.
- */
-
-int is_builtin_command(char *command)
-{
-	return (strcmp(command, "exit") == 0 || strcmp(command, "env") == 0);
-}
-
-
-/**
  * execute_builtin_command - supports the exit command
  * for exiting the shell.
  * @command: param command to be executed.
